a161581065f620b3a5ef50595de30f210b226243
RifqyYR <yudhiestrarifqy@gmail.com>
2024-01-03 11:15:40 +0800

    test update telegram

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Claim;
+use App\Models\User;
 use App\Notifications\ExampleNotification;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -19,7 +22,17 @@ class TelegramController extends Controller
 
     public function message(Request $request)
     {
-        auth()->user()->notify(new ExampleNotification($request->message));
+        $users = User::role('VERIFICATOR')->get();
+
+        foreach ($users as $user) {
+            $claims = Claim::where('completion_limit_date', Carbon::tomorrow())
+                ->get();
+
+            if ($claims->count() > 0) {
+                $message = "Halo, {$user->name}! Besok ada " . $claims->count() . " claim yang harus diverifikasi. Silahkan cek di aplikasi.";
+                $user->notify(new ExampleNotification($message));
+            }
+        }
 
         return redirect()->back();
     }
