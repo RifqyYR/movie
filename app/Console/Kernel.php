a38e34cd60b0f5fe9eb8a2518c212f2003f5a4c3
RifqyYR <yudhiestrarifqy@gmail.com>
2024-01-19 11:20:19 +0800

    test update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,13 +13,17 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        // $schedule->call(function() {
-        //     app()->make('App\Http\Controllers\TelegramController')->message();
-        // })->everyMinute()->timezone('Asia/Singapore');
+        $schedule->call(function() {
+            app()->make('App\Http\Controllers\TelegramController')->message();
+        })->everyMinute()->timezone('Asia/Singapore');
         
-        // $schedule->call(function() {
-        //     app()->make('App\Http\Controllers\TelegramController')->message_fktp();
-        // })->everyMinute()->timezone('Asia/Singapore');
+        $schedule->call(function() {
+            app()->make('App\Http\Controllers\TelegramController')->message_fktp();
+        })->everyMinute()->timezone('Asia/Singapore');
+        
+        $schedule->call(function() {
+            app()->make('App\Http\Controllers\TelegramController')->message_cashier();
+        })->everyTwoMinutes()->timezone('Asia/Singapore');
 
         $schedule->call(function() {
             app()->make('App\Http\Controllers\ArchiveController')->checkInactiveArchive();
